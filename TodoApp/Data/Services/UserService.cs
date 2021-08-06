4200cd3d686e6737018933794d918a7947ff5c7d
James Gibson <ja.gibson@hotmail.co.uk>
2021-08-06 10:38:59 +0100

    added some logging to userservice

--- TodoApp/Data/Services/UserService.cs
+++ TodoApp/Data/Services/UserService.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace TodoApp.Data.Services
 {
     public class UserService
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<UserService> _logger;
 
-        public UserService(IHttpContextAccessor httpContextAccessor)
+        public UserService(IHttpContextAccessor httpContextAccessor, ILogger<UserService> logger)
         {
             _httpContextAccessor = httpContextAccessor;
+            _logger = logger;
         }
 
         public string GetUserId()
@@ -27,7 +30,29 @@ namespace TodoApp.Data.Services
 
         private void GuardAgainstUnauthenticated()
         {
-            if (!_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            bool error = false;
+            if(_httpContextAccessor.HttpContext.User == null)
+            {
+                _logger.LogError("HttpContext.User is null");
+                error = true;
+            }
+            if (_httpContextAccessor.HttpContext.User.Identity == null)
+            {
+                try
+                {
+                    _logger.LogError(
+                        "HttpContext.User.Identity is null. Claims - {claims}",
+                        string.Join(";", _httpContextAccessor.HttpContext.User.Claims.Select(c => $"{c.Type}: {c.Value}")));
+                }catch(Exception ex)
+                {
+                    _logger.LogError(ex.Message, ex);
+                }
+                error = true;
+            }
+
+            error |= !_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+
+            if (error)
             {
                 throw new InvalidOperationException("User is not logged in.");
             }
